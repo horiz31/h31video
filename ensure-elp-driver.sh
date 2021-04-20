586b9cd5ffc196ef58a3d38f8466f47246df8f50
climr <stinsonbj@ornl.gov>
2021-04-20 09:42:51 -0400

    Update ensure-elp-driver.sh

--- ensure-elp-driver.sh
+++ ensure-elp-driver.sh
@@ -2,7 +2,7 @@
 # usage:
 #   ensure-elp-driver.sh
 #
-# Ensure that the elp camera driver is installed since it will be used by the video service to change bitrates
+# This script ensures that the elp camera driver is installed since it will be used by the video service to change bitrates
 
 DRY_RUN=false
 LOCAL=/usr/local
@@ -11,24 +11,16 @@ LINUX_UVC=${ELP_H264_UVC}/Linux_UVC_TestAP
 UVDL=https://github.com/uvdl
 SUDO=$(test ${EUID} -ne 0 && which sudo)
 
-# If the folder already exists, then just show the last commit
-
-if [ -d "${ELP_H264_UVC}" ] ; then
-	( cd ${ELP_H264_UVC} && echo -n "ELP_H264_UVC " && git log | head -1 )
-	exit 0
-fi
-
 # set -e stops execution of the script if an error is encountered
 set -e
 
-
+# if the folder does not exist, create, clone and make. If it does, then just pull and make
 if ! [ -d "${ELP_H264_UVC}" ] ; then
 	$SUDO mkdir -p $(dirname ${ELP_H264_UVC}) && $SUDO chmod a+w $(dirname ${ELP_H264_UVC})
 	( cd $(dirname ${ELP_H264_UVC}) && git clone ${UVDL}/$(basename ${ELP_H264_UVC}).git -b master && $SUDO make -C ${LINUX_UVC} )
 else
-	( cd ${ELP_H264_UVC} && git pull )
+	( cd ${ELP_H264_UVC} && git pull && $SUDO make -C ${LINUX_UVC} )
 fi
-# NB: we have satisfied the dependencies of camera-streamer for functions that we use with the above
-#( cd ${CAMERA_STREAMER} && $SUDO make dependencies )
+
 ( cd ${ELP_H264_UVC} && echo -n "ELP_H264_UVC " && git log | head -1 )
 
