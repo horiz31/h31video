9165c0d6b5a01b9cf749b047f2304b5aacb6cd3c
Brad Stinson <brad@horizon31.com>
2021-08-02 19:38:29 -0400

    updates

--- start-video.sh
+++ start-video.sh
@@ -58,30 +58,29 @@ gst-client pipeline_create h264src v4l2src device=/dev/stream1 ! "video/x-h264,w
 gst-client pipeline_create los interpipesrc listen-to=h264src block=true is-live=true allow-renegotiation=true stream-sync=compensate-ts ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink sync=false host=${LOS_HOST} port=${LOS_PORT} ${extra_los}
 gst-client pipeline_create edge interpipesrc listen-to=h264src block=true is-live=true allow-renegotiation=true stream-sync=compensate-ts ! nvv4l2decoder disable-dpb=true ! queue ! nvv4l2h264enc control-rate=1 ${encoder_bitrate}=${MAVPN_BITRATE} preset-level=1 ! 'video/x-h264,width=1280,height=720,framerate=15/1' ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=${MAVPN_HOST} port=${MAVPN_PORT} ${extra_mavpn}
 gst-client pipeline_create server interpipesrc listen-to=h264src block=true is-live=true allow-renegotiation=true stream-sync=compensate-ts ! nvv4l2decoder enable-max-performance=true disable-dpb=true ! queue ! nvv4l2h264enc control-rate=1 ${encoder_bitrate}=${VIDEOSERVER_BITRATE} preset-level=1 ! 'video/x-h264,width=1280,height=720,framerate=15/1' ! h264parse ! flvmux streamable=true ! rtmpsink location=rtmp://${VIDEOSERVER_HOST}:${VIDEOSERVER_PORT}/live/${VIDEOSERVER_ORG}/${VIDEOSERVER_STREAMNAME}
-gst-client pipeline_create atak interpipesrc listen-to=h264src block=true is-live=true allow-renegotiation=true stream-sync=compensate-ts ! nvv4l2decoder enable-max-performance=true disable-dpb=true ! queue ! nvv4l2h264enc control-rate=1 ${encoder_bitrate}=${ATAK_BITRATE} preset-level=1 ! h264parse ! mpegtsmux ! rtpmp2tpay ! udpsink host=${ATAK_HOST} port=${ATAK_PORT} ${extra_atak}
+gst-client pipeline_create atak interpipesrc listen-to=h264src block=true is-live=true allow-renegotiation=true stream-sync=compensate-ts ! nvv4l2decoder ! queue ! omxh264enc ${encoder_bitrate}=${ATAK_BITRATE} iframeinterval=60 ! h264parse ! mpegtsmux ! rtpmp2tpay ! udpsink host=${ATAK_HOST} port=${ATAK_PORT} ${extra_atak}
 
 # start source pipelines streaming
 gst-client pipeline_play h264src
-# sleep 2
 gst-client pipeline_play los
 
 #edge pipeline will play when/if n2n is started
 
-# server pipeline should be started by h31proxy 
-gst-client pipeline_play server  
+# server pipeline will be started if we can ping the video server, but ultimately this is unreliable. The connection will by monitored and managed by h31proxy 
+ 
+if [ -n "${VIDEOSERVER_HOST}" ] ; then   
+   if ping -q -c 1 -W 1 ${VIDEOSERVER_HOST} >/dev/null; then    
+       echo "Starting server pipeline"  
+       gst-client pipeline_play server  
+   else
+       echo "Not able to ping the video server, not going to try RTMP"
+   fi
+fi
 
-#if [ -n "${VIDEOSERVER_HOST}" ] ; then   
-#    if ping -q -c 1 -W 1 ${VIDEOSERVER_HOST} >/dev/null; then    
-#        echo "Starting server pipeline"  
-#        gst-client pipeline_play server  
-#    else
-#        echo "Not able to ping the video server, not going to try RTMP"
-#    fi
-# fi
-
-# start atak stream
+# start atak stream if host provided
 if [ -n "${ATAK_HOST}" ] ; then
     gst-client pipeline_play atak
 fi
+
 
 
